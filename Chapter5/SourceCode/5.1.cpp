f24122e50e5480cab0a7e4e264ec333ff2b456d5
AmoreMC <59020764+OpenQibo@users.noreply.github.com>
2020-11-13 08:08:17 +0800

    Create 5.1.cpp

---
+++ Chapter5/SourceCode/5.1.cpp
@@ -0,0 +1,14 @@
+//forloop.cpp
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    int i;
+    for (int i = 0; i < 5; ++i) {
+        cout << "C++ knows loops\n";
+
+    }
+    cout << "C++ knows when to stop;" << endl;
+    return 0;
+}
