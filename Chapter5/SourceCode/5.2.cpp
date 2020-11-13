cfbc7b758b95f293ef39ea9eed25fc0f548972b7
AmoreMC <59020764+OpenQibo@users.noreply.github.com>
2020-11-13 10:32:12 +0800

    Create 5.2.cpp

---
+++ Chapter5/SourceCode/5.2.cpp
@@ -0,0 +1,14 @@
+//nnum_test.cpp -- use numeric test  in for loop
+
+#include <iostream>
+int main()
+{
+    using  namespace std;
+    cout  <<  "Enter the string countdown value:  ";
+    int limit;
+    cin >> limit;
+    int i;
+    for  (i = limit; i; i--) //quits when i is 0;
+        cout << "i = " << i << "\n";
+    return 0;
+}
